ee3c119ea64d8fad6325b1e189377c50bb9c1a1e
Conor Hooley <cchooley@gmail.com>
2018-11-07 10:00:05 -0700

    initialized repo with component hierarchy; will add vuex

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <nav-bar></nav-bar>
+    <rockets class="container"></rockets>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import NavBar from '@/components/NavBar';
+import Rockets from '@/views/Rockets.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    Rockets,
+    NavBar,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/views/Rockets.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+    <section>
+        <new-rocket-form
+            :addRocket="addRocket">
+        </new-rocket-form>
+        <rocket-list 
+            :removeRocket="removeRocket"
+            :rockets="rockets">
+        </rocket-list>
+    </section>
+</template>
+
+<script>
+import NewRocketForm from '@/components/NewRocketForm'
+import RocketList from '@/components/RocketList'
+import API from '@/lib/API'
+
+export default {
+    name: 'rockets',
+    components: {
+        NewRocketForm,
+        RocketList,
+    },
+    data: () => ({
+        rockets: [],
+    }),
+    async mounted() {
+        this.rockets = await API.getRockets()
+    },
+    methods: {
+        addRocket(rocket) {
+            this.rockets.push(rocket) // send down to form component
+        },
+        removeRocket(rocket) {
+            const index = this.rockets.indexOf(rocket)
+            this.rockets.splice(index, 1)
+        }
+    },
+}
+</script>
+
+<style lang="css">
+
+</style>
+
