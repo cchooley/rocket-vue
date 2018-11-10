7f4c43113478b8682fe5f60240e7f05766e74216
Conor Hooley <cchooley@gmail.com>
2018-11-10 14:39:34 -0700

    Added vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
+import store from './store';
+import Vuex from 'vuex';
 
 Vue.config.productionTip = false;
 
+Vue.use(Vuex);
+
 new Vue({
-  render(h) { return h(App); },
+  store,
+  render(h) { return h(App); }
 }).$mount('#app');
